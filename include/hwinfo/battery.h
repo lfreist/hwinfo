88a74c702f11c952fa6ef706ba94f7dd3149e5b8
lfreist <freist@informatik.uni-freiburg.de>
2022-09-08 12:05:53 +0200

    battery.h

---
+++ include/hwinfo/battery.h
@@ -0,0 +1,15 @@
+// Copyright Leon Freist
+// Author Leon Freist <freist@informatik.uni-freiburg.de>
+
+#pragma once
+
+namespace hwinfo {
+
+class Battery {
+ public:
+  Battery() = default;
+  ~Battery() = default;
+};
+
+}  // namespace hwinfo
+
