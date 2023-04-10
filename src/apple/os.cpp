70ff2aa990bc025ff1714265285d7d10573c4f55
Facundo Galan <facundogalan91@gmail.com>
2023-04-10 00:54:27 -0300

    Try to fix apple compilation in C++11

--- src/apple/os.cpp
+++ src/apple/os.cpp
@@ -19,7 +19,8 @@ std::string OS::getFullName() {
   size_t size = 1024;
   std::string os_name;
   os_name.resize(size);
-  if (sysctlbyname("kern.os", static_cast<void*>(os_name.data()), &size, nullptr, 0) == 0) {
+  if (sysctlbyname("kern.os", (void*)(os_name.data()), &size, nullptr, 0) == 0) {
+    os_name.resize(size);  // trim the string to the actual size
     return os_name;
   }
   return "macOS <unknown version>";
@@ -30,7 +31,8 @@ std::string OS::getName() {
   size_t size = 1024;
   std::string os_name;
   os_name.resize(size);
-  if (sysctlbyname("kern.os", static_cast<void*>(os_name.data()), &size, nullptr, 0) == 0) {
+  if (sysctlbyname("kern.os", (void*)(os_name.data()), &size, nullptr, 0) == 0) {
+    os_name.resize(size);  // trim the string to the actual size
     return os_name;
   }
   return "macOS";
@@ -41,7 +43,8 @@ std::string OS::getVersion() {
   size_t size = 1024;
   std::string os_version;
   os_version.resize(size);
-  if (sysctlbyname("kern.osrelease", static_cast<void*>(os_version.data()), &size, nullptr, 0) == 0) {
+  if (sysctlbyname("kern.osrelease", (void*)(os_version.data()), &size, nullptr, 0) == 0) {
+    os_version.resize(size);  // trim the string to the actual size
     return os_version;
   }
   return "<unknown version>";
