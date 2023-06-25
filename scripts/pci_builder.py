c9b9984f4486da550e4610c9f8a7599733ed741e
Leon Freist <freist.leon@gmail.com>
2023-06-25 18:57:08 +0200

    started implementing a compile time PCI mapper using a python script

--- scripts/pci_builder.py
+++ scripts/pci_builder.py
@@ -8,21 +8,24 @@ We use the pci.ids file (https://pci-ids.ucw.cz/v2.2/pci.ids) licenced under BSD
 
 import sys
 from dataclasses import dataclass, field
-from typing import Generator
+from typing import Generator, Optional
 from collections import OrderedDict
 
 
 @dataclass
 class PCIDevice:
-    device_id: str = field(default_factory=str)
-    device_name: str = field(default_factory=str)
+    id: str = field(default_factory=str)
+    name: str = field(default_factory=str)
     subsystems: OrderedDict[str, str] = field(default_factory=OrderedDict)
+
+    def __repr__(self):
+        return f"{'{'}{self.id}, {self.name}"
 
 
 @dataclass
 class PCIVendor:
-    vendor_id: str = field(default_factory=str)
-    vendor_name: str = field(default_factory=str)
+    id: str = field(default_factory=str)
+    name: str = field(default_factory=str)
     devices: OrderedDict[str, PCIDevice] = field(default_factory=OrderedDict)
 
 
@@ -31,27 +34,26 @@ class PCIParser:
     def __init__(self, pci_ids_path: str):
         self.in_path = pci_ids_path
 
-    def parse(self) -> OrderedDict[str, PCIVendor]:
-        result: OrderedDict[str, PCIVendor] = OrderedDict()
-        last_vendor_id: str = ""
+    def parse(self) -> Generator[PCIVendor, None, None]:
+        last_vendor: Optional[PCIVendor] = None
         last_device_id: str = ""
         for line in self.read_lines():
             processed_line = line.strip()
-            _id, _info = processed_line.split("  ")
+            _id, _info = processed_line.split("  ", maxsplit=1)
             if line.startswith('\t'):
                 # device or subsystem
                 if line.startswith("\t\t"):
                     # subsystem
-                    result[last_vendor_id].devices[last_device_id].subsystems[_id] = _info
+                    last_vendor.devices[last_device_id].subsystems[_id] = _info
                 else:
                     # device
-                    result[last_vendor_id].devices[_id] = PCIDevice(_id, _info)
+                    last_vendor.devices[_id] = PCIDevice(_id, _info)
                     last_device_id = _id
             else:
+                if last_vendor:
+                    yield last_vendor
                 # vendor
-                result[_id] = PCIVendor(_id, _info)
-                last_vendor_id = _id
-        return result
+                last_vendor = PCIVendor(_id, _info)
 
     def read_lines(self) -> Generator[str, None, None]:
         with open(self.in_path) as f:
@@ -63,4 +65,16 @@ class PCIParser:
 
 
 if __name__ == "__main__":
-    pass
+    import os
+    path = "pci.ids"
+    if not os.path.isfile(path):
+        path = "scripts/pci.ids"
+    if not os.path.isfile(path):
+        path = "hwinfo/scripts/pci.ids"
+    else:
+        print("pci.ids file could not be found")
+        exit(1)
+    parser = PCIParser(path)
+    for vendor in parser.parse():
+        print(f"{'{'}{vendor.id}")
+
