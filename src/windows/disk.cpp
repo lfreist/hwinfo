2b787437cd0316ebc42ad1ac2feb9d2bf6bc8cc8
Leon Freist <freist.leon@gmail.com>
2023-10-16 09:56:04 +0200

    debug disks (windows)

--- src/windows/disk.cpp
+++ src/windows/disk.cpp
@@ -13,24 +13,19 @@ namespace hwinfo {
 
 // _____________________________________________________________________________________________________________________
 std::vector<Disk> getAllDisks() {
-  std::cout << "getAllDisks()" << std::endl;
   utils::WMI::_WMI wmi;
   const std::wstring query_string(
       L"SELECT Model, Manufacturer, SerialNumber, Size "
       L"FROM Win32_DiskDrive");
-  std::cout << "Running Query... ";
   bool success = wmi.execute_query(query_string);
   if (!success) {
-    std::cout << "Failed" << std::endl;
     return {};
   }
-  std::cout << "Succeeded" << std::endl;
   std::vector<Disk> disks;
 
   ULONG u_return = 0;
   IWbemClassObject* obj = nullptr;
   int disk_id = 0;
-  std::cout << "iterating..." << std::endl;
   while (wmi.enumerator) {
     wmi.enumerator->Next(WBEM_INFINITE, 1, &obj, &u_return);
     std::cout << "processing new value... ";
@@ -41,15 +36,24 @@ std::vector<Disk> getAllDisks() {
     std::cout << "Succeeded" << std::endl;
     Disk disk;
     disk._id = disk_id++;
+    std::cout << "Disk " << disk_id - 1 << std::endl;
     VARIANT vt_prop;
     obj->Get(L"Model", 0, &vt_prop, nullptr, nullptr);
+    std::cout << "Got Model" << std::endl;
     disk._model = utils::wstring_to_std_string(vt_prop.bstrVal);
+    std::cout << "Added Model" << std::endl;
     obj->Get(L"Manufacturer", 0, &vt_prop, nullptr, nullptr);
+    std::cout << "Got manufacturer" << std::endl;
     disk._vendor = utils::wstring_to_std_string(vt_prop.bstrVal);
+    std::cout << "Added manufacturer" << std::endl;
     obj->Get(L"SerialNumber", 0, &vt_prop, nullptr, nullptr);
+    std::cout << "Got SerialNumber" << std::endl;
     disk._serialNumber = utils::wstring_to_std_string(vt_prop.bstrVal);
+    std::cout << "Added SerialNumber" << std::endl;
     obj->Get(L"Size", 0, &vt_prop, nullptr, nullptr);
+    std::cout << "Got Size" << std::endl;
     disk._size_Bytes = static_cast<int64_t>(vt_prop.ullVal);
+    std::cout << "Added Size" << std::endl;
     VariantClear(&vt_prop);
     obj->Release();
     disks.push_back(std::move(disk));
