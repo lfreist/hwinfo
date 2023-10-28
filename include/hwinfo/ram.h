cc8268afdf0aa534905f1a2d39f244e9b4e78578
Leon Freist <freist.leon@gmail.com>
2023-10-28 19:26:51 +0200

    windows ram dimm

--- include/hwinfo/ram.h
+++ include/hwinfo/ram.h
@@ -7,10 +7,13 @@
 
 #include <cstdint>
 #include <string>
+#include <vector>
 
 namespace hwinfo {
 
 class RAM {
+  friend std::vector<RAM> getAllRAM();
+
  public:
   RAM();
   ~RAM() = default;
@@ -21,16 +24,22 @@ class RAM {
   HWI_NODISCARD const std::string& serialNumber() const;
   HWI_NODISCARD int64_t total_Bytes() const;
   HWI_NODISCARD int64_t frequency_Hz() const;
+  HWI_NODISCARD int id() const;
   int64_t free_Bytes();
   int64_t available_Bytes();
 
  private:
+  explicit RAM(int id);
+
   std::string _vendor{"<unknown>"};
   std::string _name{"<unknown>"};
   std::string _model{"<unknown>"};
   std::string _serialNumber{"<unknown>"};
   int64_t _total_Bytes = -1;
   int64_t _frequency_Hz = -1;
+  int _id;
 };
+
+std::vector<RAM> getAllRAM();
 
 }  // namespace hwinfo
--- src/windows/ram.cpp
+++ src/windows/ram.cpp
@@ -15,48 +15,7 @@
 namespace hwinfo {
 
 // _____________________________________________________________________________________________________________________
-RAM::RAM() {
-  utils::WMI::_WMI wmi;
-  const std::wstring query_string(
-      L"SELECT Capacity, ConfiguredClockSpeed, Manufacturer, SerialNumber, PartNumber FROM Win32_PhysicalMemory");
-  bool success = wmi.execute_query(query_string);
-  if (!success) {
-    return;
-  }
-  ULONG u_return = 0;
-  IWbemClassObject* obj = nullptr;
-  wmi.enumerator->Next(WBEM_INFINITE, 1, &obj, &u_return);
-  if (!u_return) {
-    return;
-  }
-  VARIANT vt_prop;
-  HRESULT hr;
-  hr = obj->Get(L"Manufacturer", 0, &vt_prop, nullptr, nullptr);
-  if (SUCCEEDED(hr)) {
-    _vendor = utils::wstring_to_std_string(vt_prop.bstrVal);
-  }
-  hr = obj->Get(L"partNumber", 0, &vt_prop, nullptr, nullptr);
-  if (SUCCEEDED(hr)) {
-    _model = utils::wstring_to_std_string(vt_prop.bstrVal);
-    // TODO: One expects an actual name of the RAM but wmi does not provide such a property...
-    //       The "Name"-property of WMI returns "PhysicalMemory".
-    _name = _model;
-  }
-  hr = obj->Get(L"Capacity", 0, &vt_prop, nullptr, nullptr);
-  if (SUCCEEDED(hr)) {
-    _total_Bytes = std::stoll(utils::wstring_to_std_string(vt_prop.bstrVal));
-  }
-  hr = obj->Get(L"ConfiguredClockSpeed", 0, &vt_prop, nullptr, nullptr);
-  if (SUCCEEDED(hr)) {
-    _frequency_Hz = static_cast<int64_t>(vt_prop.ulVal) * 1000 * 1000;
-  }
-  hr = obj->Get(L"SerialNumber", 0, &vt_prop, nullptr, nullptr);
-  if (SUCCEEDED(hr)) {
-    _serialNumber = utils::wstring_to_std_string(vt_prop.bstrVal);
-  }
-  VariantClear(&vt_prop);
-  obj->Release();
-}
+RAM::RAM() = default;
 
 // _____________________________________________________________________________________________________________________
 int64_t RAM::free_Bytes() {
@@ -64,13 +23,70 @@ int64_t RAM::free_Bytes() {
   if (res.empty()) {
     return -1;
   }
-  return std::stoll(res[0]) * 1024;
+  if (res.size() > _id) {
+    return std::stoll(res[_id]) * 1024;
+  }
+  return -1;
 }
 
 // _____________________________________________________________________________________________________________________
 int64_t RAM::available_Bytes() {
   // TODO: Get actual available memory size...
   return free_Bytes();
+}
+
+// _____________________________________________________________________________________________________________________
+RAM::RAM(int id) : _id(id) {}
+
+// _____________________________________________________________________________________________________________________
+std::vector<RAM> getAllRAM() {
+  utils::WMI::_WMI wmi;
+  const std::wstring query_string(
+      L"SELECT Capacity, ConfiguredClockSpeed, Manufacturer, SerialNumber, PartNumber FROM Win32_PhysicalMemory");
+  bool success = wmi.execute_query(query_string);
+  if (!success) {
+    return {};
+  }
+  ULONG u_return = 0;
+  IWbemClassObject* obj = nullptr;
+  std::vector<RAM> rams;
+  int id = 0;
+  while (wmi.enumerator) {
+    wmi.enumerator->Next(WBEM_INFINITE, 1, &obj, &u_return);
+    if (!u_return) {
+      break;
+    }
+    VARIANT vt_prop;
+    HRESULT hr;
+    RAM ram(id++);
+    hr = obj->Get(L"Manufacturer", 0, &vt_prop, nullptr, nullptr);
+    if (SUCCEEDED(hr)) {
+      ram._vendor = utils::wstring_to_std_string(vt_prop.bstrVal);
+    }
+    hr = obj->Get(L"partNumber", 0, &vt_prop, nullptr, nullptr);
+    if (SUCCEEDED(hr)) {
+      ram._model = utils::wstring_to_std_string(vt_prop.bstrVal);
+      // TODO: One expects an actual name of the RAM but wmi does not provide such a property...
+      //       The "Name"-property of WMI returns "PhysicalMemory".
+      ram._name = ram._model;
+    }
+    hr = obj->Get(L"Capacity", 0, &vt_prop, nullptr, nullptr);
+    if (SUCCEEDED(hr)) {
+      ram._total_Bytes = std::stoll(utils::wstring_to_std_string(vt_prop.bstrVal));
+    }
+    hr = obj->Get(L"ConfiguredClockSpeed", 0, &vt_prop, nullptr, nullptr);
+    if (SUCCEEDED(hr)) {
+      ram._frequency_Hz = static_cast<int64_t>(vt_prop.ulVal) * 1000 * 1000;
+    }
+    hr = obj->Get(L"SerialNumber", 0, &vt_prop, nullptr, nullptr);
+    if (SUCCEEDED(hr)) {
+      ram._serialNumber = utils::wstring_to_std_string(vt_prop.bstrVal);
+    }
+    VariantClear(&vt_prop);
+    obj->Release();
+    rams.push_back(std::move(ram));
+  }
+  return rams;
 }
 
 }  // namespace hwinfo
