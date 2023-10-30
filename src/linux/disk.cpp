0b748306fef8c080d140b0f495112e18cf0d18ef
AurelienBaraquin <113917759+AurelienBaraquin@users.noreply.github.com>
2023-10-30 21:14:01 +0100

    Disk information Linux side implementation, vendor and size added and others one still work (#66)
    * [ DISK ] -> change a bit the code structure, divide into multiple function, add disk size getter
    
    * [DISK] -> final commit local test is working
    
    ---------
    
    Co-authored-by: Leon Freist <freist@informatik.uni-freiburg.de>

--- src/linux/disk.cpp
+++ src/linux/disk.cpp
@@ -6,6 +6,7 @@
 #ifdef HWINFO_UNIX
 
 #include <fstream>
+#include <regex>
 
 #include "hwinfo/disk.h"
 #include "hwinfo/utils/filesystem.h"
@@ -13,50 +14,92 @@
 
 namespace hwinfo {
 
+// _____________________________________________________________________________________________________________________
+bool isPartition(const std::string& path) {
+    std::regex partitionRegex(R"((sda|sdb|sdc|nvme\d+n\d+)p?\d+$)");
+    return std::regex_search(path, partitionRegex);
+}
+
+// _____________________________________________________________________________________________________________________
+std::string getDiskVendor(const std::string& path) {
+  // nvme devices are in /sys/class/nvme/ and /sys/class/block/nvme*
+  // but the vendor file is only in /sys/class/nvme/
+  // so we need to check if the device is in /sys/class/block/nvme* and if so, we need to change the path
+  std::string vendor_path = path;
+  std::string::size_type nvme_pos = path.find("nvme");
+  if (nvme_pos != std::string::npos) {
+    std::string nvme_name = path.substr(nvme_pos, 5); // Exemple : "nvme0"
+    vendor_path = path.substr(0, nvme_pos - 6) + "nvme/" + nvme_name;
+  }
+  
+  std::ifstream f(vendor_path + "/device/vendor");
+  if (f.is_open()) {
+    std::string vendor;
+    getline(f, vendor);
+    utils::strip(vendor);
+    f.close();
+    return vendor;
+  }
+  
+  return "<unknown>";
+}
+
+// _____________________________________________________________________________________________________________________
+std::string getDiskModel(const std::string& path) {
+  std::ifstream f(path + "/device/model");
+  if (f) {
+    std::string model;
+    getline(f, model);
+    utils::strip(model);
+    f.close();
+    return model;
+  }
+  return "<unknown>";
+}
+
+// _____________________________________________________________________________________________________________________
+std::string getDiskSerialNumber(const std::string& path) {
+  std::ifstream f(path + "/device/serial");
+  if (f) {
+    std::string serial;
+    getline(f, serial);
+    utils::strip(serial);
+    f.close();
+    return serial;
+  }
+  return "<unknown>";
+}
+
+// _____________________________________________________________________________________________________________________
+int64_t getDiskSize_Bytes(const std::string& path) {
+  std::ifstream f(path + "/size");
+  if (f) {
+    int64_t size;
+    f >> size;
+    f.close();
+    return size * block_size;
+  }
+  return -1;
+}
+
 // =====================================================================================================================
 // _____________________________________________________________________________________________________________________
 std::vector<Disk> getAllDisks() {
   std::vector<Disk> disks;
   const std::string base_path("/sys/class/block/");
   for (const auto& entry : filesystem::getDirectoryEntries(base_path)) {
-    Disk disk;
-    std::string path(base_path + "/" + entry + "/device/");
-    if (!filesystem::exists(path)) {
+    std::string path(base_path + entry);
+    if (!filesystem::exists(path))
       continue;
-    }
-    std::ifstream f(path + "vendor");
-    if (f) {
-      getline(f, disk._vendor);
-    } else {
-      disk._vendor = "<unknown>";
-    }
-    f.close();
-    f.open(path + "model");
-    if (f) {
-      getline(f, disk._model);
-    } else {
-      disk._model = "<unknown>";
-    }
-    f.close();
-    f.open(path + "serial");
-    if (f) {
-      getline(f, disk._serialNumber);
-    } else {
-      disk._serialNumber = "<unknown>";
-    }
-    f.close();
-    utils::strip(disk._vendor);
-    utils::strip(disk._model);
-    utils::strip(disk._serialNumber);
-    disk._size_Bytes = -1;
-    /*
-    struct statvfs buf {};
-    std::string mount_path("/dev/");
-    mount_path.append(split_get_index(entry.path().string(), "/", -1));
-    if (statvfs(mount_path.c_str(), &buf)) {
-      size = static_cast<int64_t>(buf.f_bsize * buf.f_bfree);
-    }
-    */
+    if (isPartition(path)) // Check if it's a partition
+      continue;
+    Disk disk = Disk();
+    disk._vendor = getDiskVendor(path);
+    disk._model = getDiskModel(path);
+    disk._serialNumber = getDiskSerialNumber(path);
+    if (disk.empty()) // Check before get size because size is always define in /sys/class/block/...
+      continue;
+    disk._size_Bytes = getDiskSize_Bytes(path);
 
     disks.push_back(std::move(disk));
   }
