7e4d7319bf69deee4c8840c5ac050f43ae8a70b1
Shieldine <74987363+Shieldine@users.noreply.github.com>
2024-06-01 23:09:01 +0200

    remove unnecessary functions

--- src/apple/gpu.cpp
+++ src/apple/gpu.cpp
@@ -14,19 +14,6 @@
 namespace hwinfo {
 
 // _____________________________________________________________________________________________________________________
-std::string read_drm_by_path(const std::string& path) {
-  // TODO: implement
-  return "<unknown>";
-}
-
-// _____________________________________________________________________________________________________________________
-std::vector<int> get_frequencies(const std::string drm_path) {
-  std::vector<int> freqs(3);
-  // TODO: implement
-  return freqs;
-}
-
-// _____________________________________________________________________________________________________________________
 std::vector<GPU> getAllGPUs() {
   std::vector<GPU> gpus{};
   // TODO: implement
