0d065f7251db50120d6f5c81f34f3ab1e5935302
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2024-07-08 01:25:26 -0700

    feat: rewrite the example via libfmt

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,158 +1,180 @@
 // Copyright Leon Freist
 // Author Leon Freist <freist@informatik.uni-freiburg.de>
 
+#include <fmt/core.h>
 #include <hwinfo/hwinfo.h>
 
-#include <iomanip>
-#include <iostream>
+#include <cassert>
 #include <vector>
 
-#include "hwinfo/utils/PCIMapper.h"
+int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv) {
+  fmt::print(
+      "hwinfo is an open source, MIT licensed project that implements a platform independent "
+      "hardware and system information gathering API for C++.\n\n"
+      "If you face any issues, find bugs or if your platform is not supported yet, do not hesitate to create "
+      "a ticket at https://github.com/lfreist/hwinfo/issues.\n\n"
+      "Thanks for using hwinfo!\n\n");
 
-int main(int argc, char** argv) {
-  const auto width = 20;
-
-  std::cout << "hwinfo is an open source, MIT licensed project that implements a platform independent "
-            << "hardware and system information gathering API for C++.\n\n"
-            << "If you face any issues, find bugs or if your platform is not supported yet, do not hesitate to create "
-            << "a ticket at https://github.com/lfreist/hwinfo/issues.\n\n"
-            << "Thanks for using hwinfo!" << '\n';
-  std::cout << '\n' << "Hardware Report:" << '\n' << '\n';
-  std::cout << "----------------------------------- CPU -----------------------------------" << '\n';
-  auto cpus = hwinfo::getAllCPUs();
+  fmt::print(
+      "Hardware Report:\n\n"
+      "----------------------------------- CPU ------------------------------------\n");
+  const auto cpus = hwinfo::getAllCPUs();
   for (const auto& cpu : cpus) {
-    std::cout << "Socket " << cpu.id() << ":\n";
-    std::cout << std::left << std::setw(width) << " vendor:";
-    std::cout << cpu.vendor() << '\n';
-    std::cout << std::left << std::setw(width) << " model:";
-    std::cout << cpu.modelName() << '\n';
-    std::cout << std::left << std::setw(width) << " physical cores:";
-    std::cout << cpu.numPhysicalCores() << '\n';
-    std::cout << std::left << std::setw(width) << " logical cores:";
-    std::cout << cpu.numLogicalCores() << '\n';
-    std::cout << std::left << std::setw(width) << " max frequency:";
-    std::cout << cpu.maxClockSpeed_MHz() << '\n';
-    std::cout << std::left << std::setw(width) << " regular frequency:";
-    std::cout << cpu.regularClockSpeed_MHz() << '\n';
-    std::cout << std::left << std::setw(width) << " cache size (L1, L2, L3): ";
-    std::cout << cpu.L1CacheSize_Bytes() << ", " << cpu.L2CacheSize_Bytes() << ", " << cpu.L3CacheSize_Bytes() << '\n';
+    // clang-format off
+    fmt::print("Socket {}:\n"
+               "{:<20} {}\n"
+               "{:<20} {}\n"
+               "{:<20} {}\n"
+               "{:<20} {}\n"
+               "{:<20} {}\n"
+               "{:<20} {}\n"
+               "{:<20} L1: {}, L2: {}, L3: {}\n",
+               cpu.id(),
+               "vendor:", cpu.vendor(),
+               "model:", cpu.modelName(),
+               "physical cores:", cpu.numPhysicalCores(),
+               "logical cores:", cpu.numLogicalCores(),
+               "max frequency:", cpu.maxClockSpeed_MHz(),
+               "regular frequency:", cpu.regularClockSpeed_MHz(),
+               "cache size:", cpu.L1CacheSize_Bytes(), cpu.L2CacheSize_Bytes(), cpu.L3CacheSize_Bytes());
+    // clang-format on
+
     auto threads_utility = cpu.threadsUtilisation();
     auto threads_speed = cpu.currentClockSpeed_MHz();
-    for (int thread_id = 0; thread_id < threads_utility.size(); ++thread_id) {
-      std::cout << std::left << std::setw(width) << "   Thread " + std::to_string(thread_id) + ": ";
-      std::cout << threads_speed[thread_id] << " MHz (" << threads_utility[thread_id] * 100 << "%)" << '\n';
+    assert((threads_utility.size() == threads_speed.size()));
+    for (size_t thread_id = 0; thread_id != threads_utility.size(); ++thread_id) {
+      fmt::print("{:<20} Thread {}: {} MHz ({}%)\n", " ", thread_id, threads_speed[thread_id],
+                 threads_utility[thread_id] * 100);
     }
-    // std::cout << cpu.currentTemperature_Celsius() << '\n';
+    // fmt::print("{}\n", cpu.currentTemperature_Celsius());
   }
 
   hwinfo::OS os;
-  std::cout << "----------------------------------- OS ------------------------------------" << '\n';
-  std::cout << std::left << std::setw(width) << "Operating System:";
-  std::cout << os.name() << '\n';
-  std::cout << std::left << std::setw(width) << "version:";
-  std::cout << os.version() << '\n';
-  std::cout << std::left << std::setw(width) << "kernel:";
-  std::cout << os.kernel() << '\n';
-  std::cout << std::left << std::setw(width) << "architecture:";
-  std::cout << (os.is32bit() ? "32 bit" : "64 bit") << '\n';
-  std::cout << std::left << std::setw(width) << "endianess:";
-  std::cout << (os.isLittleEndian() ? "little endian" : "big endian") << '\n';
+  // clang-format off
+  fmt::print("----------------------------------- OS ------------------------------------\n"
+             "{:<20} {}\n"
+             "{:<20} {}\n"
+             "{:<20} {}\n"
+             "{:<20} {}\n"
+             "{:<20} {}\n",
+             "Operating System:", os.name(),
+             "version:", os.version(),
+             "kernel:", os.kernel(),
+             "architecture:", os.is32bit() ? "32 bit" : "64 bit",
+             "endianess:", os.isLittleEndian() ? "little endian" : "big endian");
+  // clang-format on
 
   auto gpus = hwinfo::getAllGPUs();
-  std::cout << "----------------------------------- GPU -----------------------------------" << '\n';
-  for (auto& gpu : gpus) {
-    std::cout << "GPU " << gpu.id() << ":\n";
-    std::cout << std::left << std::setw(width) << "  vendor:";
-    std::cout << gpu.vendor() << '\n';
-    std::cout << std::left << std::setw(width) << "  model:";
-    std::cout << gpu.name() << '\n';
-    std::cout << std::left << std::setw(width) << "  driverVersion:";
-    std::cout << gpu.driverVersion() << '\n';
-    std::cout << std::left << std::setw(width) << "  memory [MiB]:";
-    std::cout << static_cast<double>(gpu.memory_Bytes()) / 1024.0 / 1024.0 << '\n';
-    std::cout << std::left << std::setw(width) << "  frequency:";
-    std::cout << gpu.frequency_MHz() << '\n';
-    std::cout << std::left << std::setw(width) << "  cores:";
-    std::cout << gpu.num_cores() << '\n';
-    std::cout << std::left << std::setw(width) << "  vendor_id:";
-    std::cout << gpu.vendor_id() << '\n';
-    std::cout << std::left << std::setw(width) << "  device_id:";
-    std::cout << gpu.device_id() << '\n';
+  fmt::print("----------------------------------- GPU -----------------------------------\n");
+  for (const auto& gpu : gpus) {
+    // clang-format off
+    fmt::print("GPU {}:\n"
+                "{:<20} {}\n"
+                "{:<20} {}\n"
+                "{:<20} {}\n"
+                "{:<20} {:.2f}\n"
+                "{:<20} {}\n"
+                "{:<20} {}\n"
+                "{:<20} {}\n"
+                "{:<20} {}\n",
+                gpu.id(),
+                "vendor:", gpu.vendor(),
+                "model:", gpu.name(),
+                "driverVersion:", gpu.driverVersion(),
+                "memory [MiB]:", static_cast<double>(gpu.memory_Bytes()) / 1024.0 / 1024.0,
+                "frequency:", gpu.frequency_MHz(),
+                "cores:", gpu.num_cores(),
+                "vendor_id:", gpu.vendor_id(),
+                "device_id:", gpu.device_id());
+    // clang-format on
   }
 
   hwinfo::Memory memory;
-  std::cout << "----------------------------------- RAM -----------------------------------" << '\n';
-  std::cout << std::left << std::setw(width) << "size [MiB]:";
-  std::cout << memory.total_Bytes() / 1024 / 1024 << '\n';
-  std::cout << std::left << std::setw(width) << "free [MiB]:";
-  std::cout << memory.free_Bytes() / 1024 / 1024 << '\n';
-  std::cout << std::left << std::setw(width) << "available [MiB]:";
-  std::cout << memory.available_Bytes() / 1024 / 1024 << '\n';
+  // clang-format off
+  fmt::print("----------------------------------- RAM -----------------------------------\n"
+             "{:<20} {}\n"
+             "{:<20} {}\n"
+             "{:<20} {}\n",
+             "size [MiB]:", memory.total_Bytes() / 1024 / 1024,
+             "free [MiB]:", memory.free_Bytes() / 1024 / 1024,
+             "available [MiB]:", memory.available_Bytes() / 1024 / 1024);
+  // clang-format on
+
   for (const auto& module : memory.modules()) {
-    std::cout << "RAM " << module.id << ":\n";
-    std::cout << std::left << std::setw(width) << "  vendor:";
-    std::cout << module.vendor << '\n';
-    std::cout << std::left << std::setw(width) << "  model:";
-    std::cout << module.model << '\n';
-    std::cout << std::left << std::setw(width) << "  name:";
-    std::cout << module.name << '\n';
-    std::cout << std::left << std::setw(width) << "  serial-number:";
-    std::cout << module.serial_number << '\n';
-    std::cout << std::left << std::setw(width) << "  Frequency [MHz]:";
-    std::cout << module.frequency_Hz / 1000 / 1000 << '\n';
+    // clang-format off
+    fmt::print("RAM {}:\n"
+                "{:<20} {}\n"
+                "{:<20} {}\n"
+                "{:<20} {}\n"
+                "{:<20} {}\n"
+                "{:<20} {}\n",
+                module.id,
+                "vendor:", module.vendor,
+                "model:", module.model,
+                "name:", module.name,
+                "serial-number:", module.serial_number,
+                "Frequency [MHz]:", module.frequency_Hz / 1000 / 1000);
+    // clang-format on
   }
 
   hwinfo::MainBoard main_board;
-  std::cout << "------------------------------- Main Board --------------------------------" << '\n';
-  std::cout << std::left << std::setw(width) << "vendor:";
-  std::cout << main_board.vendor() << '\n';
-  std::cout << std::left << std::setw(width) << "name:";
-  std::cout << main_board.name() << '\n';
-  std::cout << std::left << std::setw(width) << "version:";
-  std::cout << main_board.version() << '\n';
-  std::cout << std::left << std::setw(width) << "serial-number:";
-  std::cout << main_board.serialNumber() << '\n';
+  // clang-format off
+  fmt::print("------------------------------- Main Board --------------------------------\n"
+             "{:<20}{}\n"
+             "{:<20}{}\n"
+             "{:<20}{}\n"
+             "{:<20}{}\n",
+             "vendor:", main_board.vendor(),
+             "name:", main_board.name(),
+             "version:", main_board.version(),
+             "serial-number:", main_board.serialNumber());
+  // clang-format on
 
   std::vector<hwinfo::Battery> batteries = hwinfo::getAllBatteries();
-  std::cout << "------------------------------- Batteries ---------------------------------" << '\n';
+
+  fmt::print("------------------------------- Batteries ---------------------------------\n");
   if (!batteries.empty()) {
     int battery_counter = 0;
     for (auto& battery : batteries) {
-      std::cout << "Battery " << battery_counter++ << ":" << '\n';
-      std::cout << std::left << std::setw(width) << "  vendor:";
-      std::cout << battery.vendor() << '\n';
-      std::cout << std::left << std::setw(width) << "  model:";
-      std::cout << battery.model() << '\n';
-      std::cout << std::left << std::setw(width) << "  serial-number:";
-      std::cout << battery.serialNumber() << '\n';
-      std::cout << std::left << std::setw(width) << "  charging:";
-      std::cout << (battery.charging() ? "yes" : "no") << '\n';
-      std::cout << std::left << std::setw(width) << "  capacity:";
-      std::cout << battery.capacity() << '\n';
+      // clang-format off
+      fmt::print("Battery {}:\n"
+                 "{:<20} {}\n"
+                 "{:<20} {}\n"
+                 "{:<20} {}\n"
+                 "{:<20} {}\n"
+                 "{:<20} {}\n",
+                 battery_counter++,
+                 "vendor:", battery.vendor(),
+                 "model:", battery.model(),
+                 "serial-number:", battery.serialNumber(),
+                 "charging:", battery.charging() ? "yes" : "no",
+                 "capacity:", battery.capacity());
+      // clang-format on
     }
-    std::cout << "---------------------------------------------------------------------------" << '\n';
   } else {
-    std::cout << "No Batteries installed or detected" << '\n';
+    fmt::print("No Batteries installed or detected\n");
   }
 
   std::vector<hwinfo::Disk> disks = hwinfo::getAllDisks();
-  std::cout << "--------------------------------- Disks -----------------------------------" << '\n';
+  fmt::print("--------------------------------- Disks -----------------------------------\n");
   if (!disks.empty()) {
     int disk_counter = 0;
     for (const auto& disk : disks) {
-      std::cout << "Disk " << disk_counter++ << ":" << '\n';
-      std::cout << std::left << std::setw(width) << "  vendor:";
-      std::cout << disk.vendor() << '\n';
-      std::cout << std::left << std::setw(width) << "  model:";
-      std::cout << disk.model() << '\n';
-      std::cout << std::left << std::setw(width) << "  serial-number:";
-      std::cout << disk.serialNumber() << '\n';
-      std::cout << std::left << std::setw(width) << "  size:";
-      std::cout << disk.size_Bytes() << '\n';
+      // clang-format off
+      fmt::print("Disk {}:\n"
+                 "{:<20} {}\n"
+                 "{:<20} {}\n"
+                 "{:<20} {}\n"
+                 "{:<20} {}\n",
+                 disk_counter++,
+                 "vendor:", disk.vendor(),
+                 "model:", disk.model(),
+                 "serial-number:", disk.serialNumber(),
+                 "size:", disk.size_Bytes());
+      // clang-format on
     }
-    std::cout << "---------------------------------------------------------------------------" << '\n';
   } else {
-    std::cout << "No Disks installed or detected" << '\n';
+    fmt::print("No Disks installed or detected\n");
   }
   return EXIT_SUCCESS;
 }
