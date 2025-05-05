51579eabaa38bf96362315f51ae6d3fab1f84a6b
Sergey Fedorov <vital.had@gmail.com>
2025-05-06 01:03:41 +0800

    Fix build on macOS (#122)
    * mainboard: fix for missing kIOMainPortDefault
    
    * battery: include CoreFoundation

--- src/apple/mainboard.cpp
+++ src/apple/mainboard.cpp
@@ -9,6 +9,10 @@
 
 #include "hwinfo/mainboard.h"
 
+#ifndef kIOMainPortDefault
+#define kIOMainPortDefault kIOMasterPortDefault
+#endif
+
 namespace hwinfo {
 
 std::string get_mainboard_property(CFStringRef property_name) {
