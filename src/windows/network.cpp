999ea31a2c1c72898818a1904608928e963964db
Jochen Rau <jochen.rau@inphase.no>
2026-04-28 01:59:03 +0200

    explicit cast to avoid type conversion warning

--- src/windows/network.cpp
+++ src/windows/network.cpp
@@ -1,4 +1,4 @@
-﻿#include "hwinfo/platform.h"
+#include "hwinfo/platform.h"
 
 #ifdef HWINFO_WINDOWS
 
@@ -23,7 +23,7 @@ std::vector<Network> getAllNetworks() {
   ULONG u_return = 0;
   IWbemClassObject* obj = nullptr;
   while (wmi.enumerator) {
-    wmi.enumerator->Next(WBEM_INFINITE, 1, &obj, &u_return);
+    wmi.enumerator->Next((long)WBEM_INFINITE, 1, &obj, &u_return);
     if (!u_return) {
       break;
     }
--- src/windows/ram.cpp
+++ src/windows/ram.cpp
@@ -28,7 +28,7 @@ Memory::Memory() {
   std::vector<Memory> rams;
   int id = 0;
   while (wmi.enumerator) {
-    wmi.enumerator->Next(WBEM_INFINITE, 1, &obj, &u_return);
+    wmi.enumerator->Next((long)WBEM_INFINITE, 1, &obj, &u_return);
     if (!u_return) {
       break;
     }
